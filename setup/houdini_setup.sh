e87a9de9865b11f03cbd9f7cbd9eb91a7e794dba
philnewm <Philippkd8@gmail.com>
2023-05-28 21:51:00 +0200

    init houdini setup
    added todo list

---
+++ setup/houdini_setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# This file contains a bunch of functions related to setting up houdini.
+
+install_dependencies()
+{
+    sudo dnf install libnsl -y
+}
+
+install_houdini_launcher()
+{
+    cp some_server_path
+    chmod +x installer file
+    sudo ./ installer file
+    # yes to default location
+    auto install side fx labs
+    # license agreement
+}
