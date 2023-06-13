f6f993335a82f2453dd307869dbc9f1b44d41c0e
philnewm <Philippkd8@gmail.com>
2023-06-13 22:35:11 +0200

    added initial setup commands

--- lib/gnome_utils.sh
+++ lib/gnome_utils.sh
@@ -1,6 +1,39 @@
 #! /bin/bash
 
+install_tweaks()
+{
+    sudo dnf install gnome-tweaks
+}
+
 enable_dark_mode()
 {
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+}
+
+window_config()
+{
+    org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
+    org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
+    org.gnome.mutter attach-modal-dialogs false
+}
+
+disable_extension_popup()
+{
+    org.gnome.tweaks show-extensions-notice false
+}
+
+disable_hot_corner()
+{
+    org.gnome.desktop.interface enable-hot-corners false
+}
+
+nautilus_settings()
+{
+    org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+}
+
+get_settings()
+{
+    gsettings list-recursively > /tmp/gsettings.after
+    diff /tmp/gsettings.before /tmp/gsettings.after | grep -Po '> \K.*'
 }
