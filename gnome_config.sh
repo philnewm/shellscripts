e8b12ce58c56a082c9be722d38686ff43732dd38
philnewm <Philippkd8@gmail.com>
2023-06-17 13:44:42 +0200

    added handling for user themes

--- gnome_config.sh
+++ gnome_config.sh
@@ -4,8 +4,15 @@ source lib/gnome_utils.sh
 
 setup_de()
 {
-    enable_dark_mode
     install_ui_tools
     gnome_settings
     tweaks_settings
+    nautilus_settings
+    enable_user_extension
+
+    # themes
+    install_cursor_theme
+    install_icon_theme
+    install_shell_themes
+    activate_themes
 }
--- lib/gnome_utils.sh
+++ lib/gnome_utils.sh
@@ -9,36 +9,84 @@ install_ui_tools()
 
 tweaks_settings()
 {
-    org.gnome.mutter attach-modal-dialogs false
-    
+    org.gnome.tweaks show-extensions-notice false
+
     # fonts
-    org.gnome.desktop.interface document-font-name 'Cantarell 11'
     org.gnome.desktop.interface font-name 'Cantarell Bold 11'
+    org.gnome.desktop.interface document-font-name 'Cantarell 11'
     org.gnome.desktop.interface monospace-font-name 'Source Code Pro 10'
     org.gnome.desktop.wm.preferences titlebar-font 'Carlito Bold Italic 11'
     
-    # windows
+    #keyboard mouse
+    org.gnome.mutter locate-pointer-key 'Control_L'
+
+    #top bar
+    org.gnome.desktop.interface clock-show-weekday true
+
+    # title bars
     org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
     org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
 
-    # theme
-    sudo dnf install gtk-murrine-engine -y
-    sudo dnf install sassc -y
-    git https://github.com/vinceliuice/Lavanda-gtk-theme.git /tmp/theme/
-    cd /tmp/theme || exit 1
-    ./install.sh
-
+    # windows
+    org.gnome.mutter attach-modal-dialogs false
 }
 
-disable_extension_popup()
+enable_user_extension()
 {
-    org.gnome.tweaks show-extensions-notice false
+    sudo dnf install gnome-shell-extension-user-theme -y
+    gnome-session-quit
+}
+
+activate_themes()
+{
+    org.gnome.desktop.interface cursor-theme 'Qogir'
+    org.gnome.desktop.interface icon-theme 'Tela-circle-purple-dark'
+    org.gnome.shell.extensions.user-theme name 'Lavanda-Dark'
+    org.gnome.desktop.interface gtk-theme 'Lavanda-Dark'
+}
+
+install_shell_themes()
+{    
+    sudo dnf install gtk-murrine-engine -y
+    sudo dnf install sassc -y
+    git clone https://github.com/vinceliuice/Lavanda-gtk-theme.git /tmp/theme/
+    cd /tmp/theme || exit 1
+    ./install.sh
+    cd ..
+    rm -fR theme
+}
+
+install_icon_theme()
+{
+    git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git /tmp/icon_theme
+    cd /tmp/icon_theme || exit 1
+    ./install.sh purple
+    cd ..
+    rm -fR icon_theme
+}
+
+install_cursor_theme()
+{
+    git clone https://github.com/vinceliuice/Qogir-icon-theme.git /tmp/cursor_theme
+    cd /tmp/cursor_theme || exit 1
+    ./install.sh
+    cd ..
+    rm -fR cursor_theme
 }
 
 gnome_settings()
 {
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     org.gnome.desktop.interface enable-hot-corners false
+
+    # file history
+    org.gnome.desktop.privacy remember-recent-files true
+    org.gnome.desktop.privacy recent-files-max-age -1
+
+    #trash and temp settings
+    org.gnome.desktop.privacy remove-old-trash-files true
+    org.gnome.desktop.privacy remove-old-temp-files true
+    org.gnome.desktop.privacy old-files-age uint32 30
 }
 
 nautilus_settings()
