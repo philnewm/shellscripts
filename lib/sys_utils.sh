ffe3f610206d3f11b1385fd1ad81d4a61d1db15e
philnewm <Philippkd8@gmail.com>
2023-06-18 21:18:46 +0200

    added flathub + rpm fusion repos

--- lib/sys_utils.sh
+++ lib/sys_utils.sh
@@ -14,5 +14,25 @@ enable_selinux_temporarily()
     echo "[OK] Set se_linux to enforce temporarely"
 }
 
+update_system() 
+{
+	sudo dnf update -y
+}
+
+enable_rpm_fusion()
+{
+    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
+    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
+}
+
+enable_flathub()
+{
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
+
 # TODO change SE Linux Settings permanent
 # sudo /etc/selinux/config change SELINUX=enforcing to SELINUX=permissive
+
+# TODO check for rpm fusion repo
+# TODO change dnf defaults
+# https://itsfoss.com/things-to-do-after-installing-fedora/
--- system_setup.sh
+++ system_setup.sh
@@ -2,4 +2,9 @@
 
 source mount_network_shares.sh
 
+update_system
+enable_rpm_fusion
+install_nvidia_driver
+# set basic default configs for gnome (tweaks)
+# set default settings for firefox
 mount_shares
