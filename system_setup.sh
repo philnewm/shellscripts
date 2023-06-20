44d02920f2308681df13bac98fef2c2b3421b2ea
philnewm <Philippkd8@gmail.com>
2023-06-20 22:15:02 +0200

    added ideas for auto reboot

--- system_setup.sh
+++ system_setup.sh
@@ -6,8 +6,20 @@ source lib/driver.sh
 
 # TODO check why sudo -i coesn't wqork right away
 
-update_system
-install_nvidia_driver
+start_state="state_start"
+initial_update_state="state_initial_update"
+gpu_driver_installed_state="state_driver_installed"
+theme_extension_installed="state_theme_extension"
+
+if [ ! -e "state_start" ];
+then
+    update_system
+    install_nvidia_driver
+    # TODO write into cron tab and remove after execution again
+    # TODO check outo login enable/disable
+    # TODO change default grub config for faste reboot
+fi
+
 # set basic default configs for gnome (tweaks)
 # set default settings for firefox
 mount_shares
