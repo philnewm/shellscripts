54fa93f2ba87729d8af0c3e250599a838d46b6cc
philnewm <Philippkd8@gmail.com>
2023-06-25 17:58:57 +0200

    removed update command from driver

--- src/sys_setup/driver.sh
+++ src/sys_setup/driver.sh
@@ -13,8 +13,6 @@ install_nvidia_driver()
 
 	if echo "$(sudo lshw -c "video" | grep "vendor")"="NVIDIA Coporation";
 	then
-		sudo dnf update -y
-
 		sudo dnf install akmod-nvidia -y
 		sudo dnf install xorg-x11-drv-nvidia-cuda -y
 		return 0
