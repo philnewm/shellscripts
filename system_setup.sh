bb7fc97c0d72e9be1be648e3719604da7aa563bd
philnewm <Philippkd8@gmail.com>
2023-06-25 17:24:49 +0200

    updated system_setup.sh for dev branch

--- system_setup.sh
+++ system_setup.sh
@@ -3,16 +3,38 @@
 source src/network_setup/mount_network_shares.sh
 source src/sys_setup/driver.sh
 
-# TODO check why sudo -i doesn't work right away
+# TODO check why sudo -i coesn't work right away
+tmp_state_path="state/"
+gpu_driver_installed_state="state_driver_installed"
+theme_extension_installed="state_theme_extension"
 
-sudo dnf update -y
-enable_rpm_fusion
-install_nvidia_driver
-# change_host_name "Fedora Machine"
-# change_dnf_settings
-# set basic default configs for gnome (tweaks)
+if [ ! -e "$($tmp_state_path$gpu_driver_installed_state)" ] && [ ! -e "$($tmp_state_path$theme_extension_installed)" ];
+then
+    sudo dnf update -y
+    enable_rpm_fusion
+    install_nvidia_driver
+    # TODO check auto login enable/disable
+    # TODO change default grub config for faster reboot
+    # change_host_name "Fedora Machine"
+    # change_dnf_settings
+    mount_shares
+    touch "$($tmp_state_path$gpu_driver_installed_state)"
+    sudo reboot
+fi
+
+if [ -e "$($tmp_state_path$gpu_driver_installed_state)" ] && [ ! -e "$($tmp_state_path$theme_extension_installed)" ];
+then
+    setup_gnome_environment
+    touch "$($tmp_state_path$theme_extension_installed)"
+    sudo systemctl restart gdm
+fi
+
+if [ -e "$($tmp_state_path$gpu_driver_installed_state)" ] && [ -e "$($tmp_state_path$theme_extension_installed)" ];
+then
+    activate_themes
+fi
+
 # set default settings for firefox
-mount_shares
 # install nomachine -> maybe apply settings automatically
 # install extensions for DE
 # setup environment variables
